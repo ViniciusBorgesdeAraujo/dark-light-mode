b8bb854cea79a7394de1cc654a8457abf9276481
Vinicius Borges <vinnepaul@gmail.com>
2022-09-08 13:52:01 -0300

    first commit

---
+++ my-app/src/App.js
@@ -0,0 +1,21 @@
+import React, { useState } from "react";
+import "./styles.css";
+
+//☀︎ ☽
+export default function App() {
+  const [darkMode, setDarkMode] = useState(false);
+  return (
+    <div className={darkMode ? "dark-mode" : "light-mode"}>
+      <div className="container">
+        <span style={{ color: darkMode ? "grey" : "yellow" }}>☀︎</span>
+        <div className="switch-checkbox">
+          <label className="switch">
+            <input type="checkbox" onChange={() => setDarkMode(!darkMode)} />
+            <span className="slider round"> </span>
+          </label>
+        </div>
+        <span style={{ color: darkMode ? "#c96dfd" : "grey" }}>☽</span>
+      </div>
+    </div>
+  );
+}
